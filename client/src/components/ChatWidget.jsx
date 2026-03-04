a9dd21991bbf15f75242ac8a04d958ecce487166
Kishor MB <kishor@example.com>
2026-03-04 18:57:30 +0530

    Updated frontend icon

--- client/src/components/ChatWidget.jsx
+++ client/src/components/ChatWidget.jsx
@@ -240,9 +240,9 @@ export default function ChatWidget() {
             <div className="flex items-center gap-3">
       <div className="w-10 h-10 bg-white/20 rounded-full flex items-center justify-center backdrop-blur-sm p-1">
     <img 
-      src="Images\Left_logo.png"
+      src="C:\Users\Win\OneDrive\Desktop\shiksha\client\public\Fav.png"
       alt="Shiksha Source Logo"
-      className="w-8 h-8 object-contain"
+      className="w-6 h-6 object-contain"
     />
   </div>
   <div>
