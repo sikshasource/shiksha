d21e8e036a5d59691b55873226b1f15f4293ec48
Nachiketa NR <nrnachi34@example.com>
2026-03-08 14:50:21 +0530

    Fixed the cors config

--- client/src/components/CustomProject.jsx
+++ client/src/components/CustomProject.jsx
@@ -31,7 +31,7 @@ const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
      try {
-      const response = await fetch("http://localhost:8000/api/custom-project", {
+      const response = await fetch("https://shiksha-l2iu.onrender.com/api/custom-project", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
